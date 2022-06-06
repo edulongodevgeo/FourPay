ec795892703e4a695f0615b75f35f4006fe5879d
Tiago Felix Prudente <tiago.prudente@foursys.com.br>
2022-06-06 11:05:46 -0300

    Added started files

---
+++ src/main/java/br/com/foursys/fourpay/fourpay/FourPayApplication.java
@@ -0,0 +1,13 @@
+package br.com.foursys.fourpay.fourpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/foursys/fourpay/fourpay/FourPayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foursys.fourpay.fourpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
