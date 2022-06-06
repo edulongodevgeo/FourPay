d16b951395694b8474152a5d431ad8c6de875a18
Tiago Felix Prudente <tiago.prudente@foursys.com.br>
2022-06-06 15:45:06 -0300

    Made some enums

--- src/main/java/br/com/foursys/fourpay/enums/ClientType.java
+++ src/main/java/br/com/foursys/fourpay/enums/ClientType.java
@@ -1,5 +1,21 @@
 package br.com.foursys.fourpay.enums;
 
 public enum ClientType {
+    COMUM(0),
+    SUPER(1),
+    PREMIUM(2);
 
+    private Integer id;
+
+    ClientType(int id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
--- src/main/java/br/com/foursys/fourpay/enums/PixKeyType.java
+++ src/main/java/br/com/foursys/fourpay/enums/PixKeyType.java
@@ -1,5 +1,21 @@
 package br.com.foursys.fourpay.enums;
 
 public enum PixKeyType {
+    CPF(0),
+    EMAIL(1),
+    TELEFONE(2),
+    ALEATORIO(3);
 
+    private Integer id;
+    PixKeyType(int id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
