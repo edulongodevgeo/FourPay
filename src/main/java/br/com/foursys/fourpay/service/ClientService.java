930f80b3ed44857c76f772a637a602689ef29ace
Tiago Felix Prudente <tiago.prudente@foursys.com.br>
2022-06-15 18:05:12 -0300

    Authentication done

--- src/main/java/br/com/foursys/fourpay/service/ClientService.java
+++ src/main/java/br/com/foursys/fourpay/service/ClientService.java
@@ -13,21 +13,50 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    public List<Client> getAllClient(){
+    public List<Client> getAllClient() {
         return clientRepository.findAll();
     }
-    public Optional<Client> getClientId(Integer id){
+
+    public Optional<Client> getClientId(Integer id) {
         return clientRepository.findById(id);
     }
-    public Client addClient(Client client){
+
+    public Client addClient(Client client) {
+        List<Client> clients = clientRepository.findAll();
+        for (Client item : clients) {
+            if (item.getCpf().equals(client.getCpf())) {
+                return null;
+            }
+        }
         return clientRepository.save(client);
     }
-    public void deleteClient(Integer id){
+
+    public void deleteClient(Integer id) {
         clientRepository.deleteById(id);
     }
-    public Client updateClient(Integer id, Client client){
+
+    public Client updateClient(Integer id, Client client) {
         client.setId(id);
         return clientRepository.save(client);
     }
 
+    public Client getClientByEmailAndPassword(String email, String password) {
+        List<Client> clientList = getAllClient();
+        for (Client client : clientList) {
+            if (client.getEmail().equals(email) && client.getPassword().equals(password)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    public String getClientByEmail(String email) {
+        List<Client> clientList = getAllClient();
+        for (Client client : clientList) {
+            if (client.getEmail().equals(email)) {
+                return client.getPassword();
+            }
+        }
+        return null;
+    }
 }
